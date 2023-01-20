91991586f917ae450a71107fffa5862f4a17acc3
vladimir <vladsto17@gmail.com>
2023-01-20 18:02:05 +0100

    solution 94

---
+++ problems/0094-binary-tree-inorder/solution.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+using std::vector;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result = {};
+        traverse(root, &result);
+        return result;
+    }
+
+    void traverse(TreeNode *node, vector<int> *vec)
+    {
+        if (!node)
+            return;
+        traverse(node->left, vec);
+        vec->push_back(node->val);
+        traverse(node->right, vec);
+    }
+};
